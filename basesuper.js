d544d39be8b08e5760d3d1f957573209049bda14
FabioVergani <fabio.vergani.1975@gmail.com>
2015-04-12 21:23:37 +0200

    Create basesuper.js

---
+++ basesuper.js
@@ -0,0 +1,28 @@
+javascript:'use strict';void((function(w){
+
+ var ns='fv',e=w[ns],k=typeof(e),Obj=Object;
+
+ if(k==='object' && 'use' in e && Obj.isExtensible(e)===false){
+	k=2;
+ }else{
+	if(k==='undefined'||Obj.getOwnPropertyDescriptor(w,ns).writable===true){
+	 e={value:0,writable:true};
+	 e=Obj.preventExtensions(Obj.create(null,{
+		use:{
+		 value:function(i){
+			console.log(i);console.dir(e);
+		 }
+		},
+		calls:e,
+		run:e
+	 }));
+	 Obj.defineProperty(w,ns,{value:e});
+	 k=1;
+	};
+ };
+
+ if(k===1||k===2){
+	e.use(k);
+ };
+
+})(window));
