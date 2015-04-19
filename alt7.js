ea37322f73b83b530216b83d86d188a2ec7cd4ff
FabioVergani <fabio.vergani.1975@gmail.com>
2015-04-19 22:43:34 +0200

    Update alt7.js

--- alt7.js
+++ alt7.js
@@ -1,18 +1,25 @@
 javascript:'use strict';void((function(thewindow){
- var o,w=thewindow,e=w.fv,k1='undercursor',k2='selectedbyclick';
+ var o,w=thewindow,e=w.fv;
  ((typeof(e)==='object' && 'use' in e)?e:(o=Object,o=w.fv=o.preventExtensions(o.create(null,(o={value:0,writable:true},{
 
 	use:{
 	 value:function(awindow){
-		var ø=o,win=awindow,d=win.document,s=d.readyState,l='__proto__',
+		var ø=o,win=awindow,d=win.document,s='style',l='__proto__',k1='undercursor',k2='selectedbyclick',
 		play=function(w,n){
 
-		 console.log(n);
+		 var g=w.fv;g.run=1;g.calls++;
+		 console.log(n);console.dir(g);
+		 g.run=0;
 
 		},
 		f=function(i){var ƒ=play;Object.defineProperty(ø,'use',{value:ƒ});ƒ(win,i);};
 		ø.fn[l]=ø.events[l]=null;
-		if(s==='complete'){f(2);}else if(ø.run===0){ø.run=1;s='DOMContentLoaded';
+
+		s=d.createElement(s);
+		s.innerHTML=['.'+k1+':not(.','){cursor:crosshair;outline:2px solid #5166bb;}.','{cursor:initial;outline:2px outset red;}'].join(k2);
+		d.head.appendChild(s);
+
+		if(d.readyState==='complete'){f(2);}else if(ø.run===0){ø.run=1;s='DOMContentLoaded';
 			l=function(){d.removeEventListener(s,l);ø.run=0;f(1);};
 			d.addEventListener(s,l);
 		};
