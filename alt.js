6efd32e6e51cf6d90a736156ea5d99c6547ec48b
FabioVergani <fabio.vergani.1975@gmail.com>
2015-04-19 12:18:04 +0200

    Update alt.js

--- alt.js
+++ alt.js
@@ -1,8 +1,14 @@
 javascript:'use strict';void((function(thewindow){
- var w=thewindow, e=w.fv, fn='use', result=(
- (typeof(e)==='object' && fn in e)?e[fn]:(w.fv=function(x){return x;})
- )(w);
+ var o={value:0,writable:true},w=thewindow, e=w.fv;
+ o=((typeof(e)==='object' && 'use' in e)?e:(w.fv=Object.preventExtensions(Object.create(null,{
+	use:{
+	 value:function(doc){}
+	},
+	calls:o,
+	run:o
+ })))).use(w);
 
- console.dir(w.fv);
+ console.log(o,w.fv);
 
 })(window));
+//-------------
