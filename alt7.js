c390e87064984f648cf05e07c561050c3dbe200a
FabioVergani <fabio.vergani.1975@gmail.com>
2015-04-19 22:14:56 +0200

    Create alt7.js

---
+++ alt7.js
@@ -0,0 +1,35 @@
+javascript:'use strict';void((function(thewindow){
+ var o,w=thewindow,e=w.fv,k1='undercursor',k2='selectedbyclick';
+ ((typeof(e)==='object' && 'use' in e)?e:(o=Object,o=w.fv=o.preventExtensions(o.create(null,(o={value:0,writable:true},{
+
+	use:{
+	 value:function(awindow){
+		var ø=o,win=awindow,d=win.document,s=d.readyState,l='__proto__',
+		play=function(w,n){
+
+		 console.log(n);
+
+		},
+		f=function(i){var ƒ=play;Object.defineProperty(ø,'use',{value:ƒ});ƒ(win,i);};
+		ø.fn[l]=ø.events[l]=null;
+		if(s==='complete'){f(2);}else if(ø.run===0){ø.run=1;s='DOMContentLoaded';
+			l=function(){d.removeEventListener(s,l);ø.run=0;f(1);};
+			d.addEventListener(s,l);
+		};
+	 },
+	 configurable:true,
+	 writable:true
+	},
+
+	calls:o,
+	run:o,
+	fn:{
+	 value:(o={
+		mouseover:function(x){},
+		mouseout:function(x){},
+		click:function(x){}
+	 },o)
+	},
+	events:{value:Object.keys(o)}
+ }))))).use(w,3);
+})(window));
