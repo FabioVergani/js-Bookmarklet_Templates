745b7473a01b967ea458f43132bc42402aadf979
FabioVergani <fabio.vergani.1975@gmail.com>
2015-04-19 13:08:50 +0200

    Update alt.js

--- alt.js
+++ alt.js
@@ -1,14 +1,23 @@
 javascript:'use strict';void((function(thewindow){
- var o={value:0,writable:true},w=thewindow, e=w.fv;
- o=((typeof(e)==='object' && 'use' in e)?e:(w.fv=Object.preventExtensions(Object.create(null,{
+ var O,o={value:0,writable:true},w=thewindow,e=w.fv;
+ O=((typeof(e)==='object' && 'use' in e)?e:(O=Object,o=w.fv=O.preventExtensions(O.create(null,{
 	use:{
-	 value:function(doc){}
-	},
-	calls:o,
-	run:o
- })))).use(w);
+	 value:function(awindow){
+		var d=awindow.document,s='complete',f=function(n){
 
- console.log(o,w.fv);
+		 var obj=o;
+		 obj.calls++;
 
+		 return n;
+		};
+		if(d.readyState===s){
+			f(1);
+		}else{
+			s='DOMContentLoaded';
+			f=function(){d.removeEventListener(s,f);f(2);};
+			d.addEventListener(s,f);
+		};
+
+ }},calls:o,run:o})))).use(w);
 })(window));
 //-------------
