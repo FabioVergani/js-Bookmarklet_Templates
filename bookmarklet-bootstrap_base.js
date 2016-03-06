182ceb3abc10812b0ca4779c62fa02e933b76aab
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-06 13:28:25 +0100

    Create bookmarklet-bootstrap_base.js

---
+++ bookmarklet-bootstrap_base.js
@@ -0,0 +1,14 @@
+javascript:void(function($){'use strict';
+ var w=$,e=w.ƒ||(w.ƒ=(function($){var w=$,o={
+	requiredmethodId1:222
+ };o.__proto__=null;return o;})(w)),i='functionId';
+ (e[i]||(e[i]=(function($){console.log('firstrun');
+	var w=$,e=w.ƒ,o,i;
+	o=e[i='requiredmethodId1']||(e[i]=666);
+	o=e[i='requiredmethodId2']||(e[i]=999);
+	return function($){
+	 var w=$;
+	 console.dir(w.ƒ);
+	};
+ })(w)))(w);
+}(window));
