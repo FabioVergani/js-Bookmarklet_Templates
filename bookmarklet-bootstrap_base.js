2df4c414fbc66fa941f52a66af40ce785b2b64fa
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-06 15:36:45 +0100

    Update bookmarklet-bootstrap_base.js

--- bookmarklet-bootstrap_base.js
+++ bookmarklet-bootstrap_base.js
@@ -1,13 +1,13 @@
 javascript:void(function($){'use strict';
- var w=$,e=w.ƒ||(w.ƒ=(function($){var w=$,o={
-	//requiredmethodId1:222
- };o.__proto__=null;return o;})(w)),i='functionId';
- (e[i]||(e[i]=(function($){
+ var w=$,i='functionId',e=w.ƒ||(w.ƒ={});
+ (e[i]||(e[i]=(function($,s){//console.log('first-run');
 	var w=$,e=w.ƒ,o,i;
+	//console.log('check-methods');
+	//o=e[i='requiredmethodId1']||(e[i]=666);
 	//o=e[i='requiredmethodId2']||(e[i]=999);
 	return function($){
 	 var w=$;
 	 //console.dir(w.ƒ);
 	};
- })(w)))(w);
+ })(w,i)))(w);
 }(window));
