4cdacad66fd791bef2d1da3904fe21fb5e6bcdcd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-06 12:45:00 +0100

    Update and rename TEMPLATEFINALE.js to aTEMPLATE.js

--- aTEMPLATE.js
+++ aTEMPLATE.js
@@ -1,5 +1,5 @@
 javascript:void(function(w){'use strict';
- var $=w,i='domSnapshot',o=$.ƒ,f=Object.create;
+ var $=w,i='zzz',o=$.ƒ,f=Object.create;
  o=o||((o={})[i]={value:0,writable:true},$.ƒ=f(null,o));
  if((f=o[i])===0){
 	f=o[i]=(function(global){
