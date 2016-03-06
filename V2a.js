ae5d55f1a291fe37404bacde624fbef9e69a7ec3
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-06 20:27:14 +0100

    Update V2a.js

--- V2a.js
+++ V2a.js
@@ -1,13 +1,16 @@
 javascript:void(function($){'use strict';
- var w=$,i='functionId',e=w.ƒ||(w.ƒ={});
+ var w=$,i='zzz',e=w.ƒ||(w.ƒ={});
  (e[i]||(e[i]=(function($,s){
 	var w=$,e=w.ƒ,i,
 	Call=e[i='Call']||(e[i]=w.Function.prototype.call),
 	Slice=e[i='Slice']||(e[i]=Call.bind(w.Array.prototype.slice)),
 	Create=e[i='Create']||(e[i]=Call.bind(w.Object.create,null,null));
 	return function($){
-	 var w=$,ƒ=this;
-	 console.dir(ƒ);
+	 var ƒ=this,w=$,d=w.document,f=function(evt){
+		console.dir(ƒ);
+		console.dir(evt);
+	 };
+	if(d.readyState==='complete'){f(false);}else{w.addEventListener('load',f);};
 	}.bind(e);
  })(w,i)))(w);
 }(window));
