35b3aaae6daa2137e8ef2ddb2ddc43ebf9790832
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-06 15:30:48 +0200

    Create logic003.js

---
+++ logic/logic003.js
@@ -0,0 +1,13 @@
+javascript:void(function(w,n,s){'use strict';
+ var o=w,p=n;
+ ((o=o[p]||(console.clear(),function(w,n){
+	var o=w, p=n;
+	return o[p]={};
+ })(o,p))[p=s]||(o[p]=function(xxx){
+	 console.log('run:',xxx);
+ }))(12345678);
+}(window,'framework_id','bookmarklet_id'));
+//===================================================
+/*
+jspef: https://jsperf.com/hasownproperty-vs-in-vs-undefined/110
+*/
