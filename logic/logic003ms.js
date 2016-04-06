6913869a4aca913764617820acb17f6ce125f5c3
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-06 16:06:38 +0200

    Update logic003ms.js

--- logic/logic003ms.js
+++ logic/logic003ms.js
@@ -1,11 +1,9 @@
 javascript:void(function(w){'use strict';
- var $=w,o,p;
- ((o=$[p='framework_id']||(function(w,n){
+ var o=w,p='framework_id';
+ ((o=o[p]||(function(w,n){
 	var o=w, p=n;
-	return o[p]={
-	 calls:0,
-	};
+	return o[p]={};
  })(o,p))[p='bookmarklet_id']||(o[p]=function(xxx){
-	 console.dir(xxx);
- }))($);
+	 console.log('run:',xxx);
+ }))(12345678);
 }(window));
