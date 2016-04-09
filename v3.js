1c37cee26f08900e762aa7ac5889bdbf205130c4
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-09 18:45:52 +0200

    Create v3.js

---
+++ v3.js
@@ -0,0 +1,28 @@
+javascript:void(function($){'use strict';
+ var w=$,o=w,p;
+ ((o=o[p='framework_id']||(function($,id_framework){
+	var w=$,
+	d=w.document,
+	Call=w.Function.prototype.call,
+
+	at=function(x,s,f,b){var e=x,i=function(o){f(o);e.removeEventListener(s,i);};e.addEventListener(s,i,b===true);},
+
+	Create=w.Object.create,
+	aRawObject=Call.bind(Create,null,null),
+	aRawObjectDescribed=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(w.Object.freeze((function(u){var c=Create,f=aRawObject,e=f(),o={value:true,writable:true,configurable:false,enumerable:true},p;for(p in o){e[p]=c(o)};e.value.value=u;return {t:e,f:f};})())),
+
+	ƒ=w[id_framework]=aRawObject(),
+
+	endvar;
+	return ƒ;
+ })(o,p))[p='bookmarklet_id']||(o[p]=function($){
+
+	var w=$,
+	scope=this,
+	d=w.document,
+	endvar;
+
+	console.dir(scope);
+
+ }.bind(o)))(w);
+}(window));
