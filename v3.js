ade73dc3e07ae8f74257db2eea01408880fefe99
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-09 19:45:51 +0200

    Update v3.js

--- v3.js
+++ v3.js
@@ -1,6 +1,6 @@
 javascript:void(function($){'use strict';
  var w=$,o=w,p;
- ((o=o[p='framework_id']||(function($,id_framework){
+ ((o=o[p='framework_id'+Date.now()]||(function($,id_framework){
 
 	var w=$,
 	d=w.document,
@@ -14,10 +14,11 @@ javascript:void(function($){'use strict';
 	aRawObject=Call.bind(Create,null,null),
 	aRawObjectDescribed=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(w.Object.freeze((function(u){var c=Create,f=aRawObject,e=f(),o={value:true,writable:true,configurable:false,enumerable:true},p;for(p in o){e[p]=c(o)};e.value.value=u;return {t:e,f:f};})())),
 
-	ƒ=w[id_framework]=aRawObject(),
+	o=w[id_framework]=aRawObject(),
 
 	endvar;
-	return ƒ;
+	log(p);
+	return o;
  })(o,p))[p='bookmarklet_id']||(o[p]=function($){
 
 	var w=$,
