5f86dce3a3654bb958960f8b2f321481a891b4e2
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-05-08 22:28:03 +0200

    Update simplebookmarklet-x2.js

--- simplebookmarklet-x2.js
+++ simplebookmarklet-x2.js
@@ -15,42 +15,34 @@ void(function($){'use strict';
 	console=w.console,
 	log=Freeze(Call.bind(console.log,console)),
 	sealed=Seal(aRawObjDescribed({
-	 opened:{value:null},
 	 locked:{value:false},
-	 go:{value:function($){
-		 var w,d,f,o=sealed;
+	 opened:{value:null},
+	 go:{
+		writable:false,
+		value:function($){
+		 var o=sealed,f,w;
 		 if(o.locked===false){
 			o.locked=true;
-			w=$;
-			d=w.document;
-			f=Freeze(function(evt){
-			 var s='about:blank',w2=o.opened,d1=d,d2,e;
-
-			 if(w2!==null){
-				w2.location=s;
-			 }else{
-				w2=o.opened=on(w.open(s),'beforeunload',function(evt){o.opened=null;});
-			 };
+			f=function(evt){
+			 var w1=evt.currentTarget,d1=w1.document,w2=o.opened,d2, s='about:blank',e=d1.createDocumentFragment().appendChild(d1.documentElement.cloneNode(true));
+			 e.normalize();
+			 if(w2===null||w2.closed){w2=o.opened=on(w1.open(s),'beforeunload',function(){o.opened=null;});};
 			 d2=w2.document;
 			 while(e=d2.firstChild){d2.removeChild(e);};
-			 d2.appendChild(d1.doctype.cloneNode());
+			 d2.appendChild((e=d1.doctype)!==null?e.cloneNode():d1.implementation.createDocumentType('html','',''));
+			 if(e=d1.documentElement){
+				e=d1.createDocumentFragment().appendChild(e.cloneNode(true));
+				e.normalize();
 
-			 e=d1.createDocumentFragment().appendChild(d1.documentElement.cloneNode(true));
-			 e.normalize();
-
-			 d2.appendChild(e);
-
-
-console.dir(d2.childNodes);
-
-
+				d2.appendChild(e);
+			 };
+			 o.locked=e=false;
 			 w2.focus();
-			 o.locked=false;
-			});
-			if(d.readyState==='complete'){f(false);}else{once(w,'load',f);};
+			};
+			w=$;
+			if(w.document.readyState==='complete'){f({currentTarget:w});}else{once(w,'load',f);};
 		 };
-		},
-		writable:false
+		}
 	 }
 	}));
 	return sealed.go;
