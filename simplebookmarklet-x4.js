700e9c090572e689144b7f205a10e08e18719df1
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-05-15 18:21:36 +0200

    Update simplebookmarklet-x4.js

--- simplebookmarklet-x4.js
+++ simplebookmarklet-x4.js
@@ -8,7 +8,7 @@ void(function($){'use strict';
 	aRawObjDescribed=Freeze(function(k){const o=k,f=aRawObj,d=f(),t=this;var p,i,e;for(p in o){i=d[p]=f(t);for(p in (e=o[p])){i[p]=e[p];};};return f(d);}.bind(function(u){var p=true;const f=Create,e=aRawObj(),s=Seal,o=s({value:p,writable:p,enumerable:p,configurable:false});for(p in o){e[p]=f(o);};e[p='value'][p]=u;return s(e);}())),
 	on=Freeze(function(o,s,f,b){const e=o;e.addEventListener(s,f,b===true);return {target:e,listener:f};}),
 	once=Freeze(function(o,s,f,b){const i=Freeze(function(x){f(x);o.removeEventListener(s,i);});on(s,i,b);}),
-	inibite=function(evt){const e=evt;e.preventDefault();e.stopPropagation();return false;},
+	inibite=Freeze(function(evt){const e=evt;e.preventDefault();e.stopPropagation();return false;}),
 	console=w.console,log=Freeze(Call.bind(console.log,console)),
 	sealed=Seal(aRawObjDescribed({opened:{value:null},locked:{value:false},go:{writable:false,value:Freeze(function($){const o=sealed;if(o.locked===false){const w=$,d=w.document;o.locked=true;const f=process;if(d.readyState==='complete'){f({currentTarget:w});}else{once(w,'load',f);};};})}})),
 	process=Freeze(function(evt){
@@ -17,11 +17,7 @@ void(function($){'use strict';
 	 if(e!==null){
 		e=d1.createDocumentFragment().appendChild(e.cloneNode(true));
 		e.normalize();
-		const dE=e,w2=((e=o.opened)===null||e.closed)?on((o.opened=w1.open('about:blank')),'beforeunload',function(){o.opened=null;}).target:e,
-		stopclick=on(w2,'click',inibite).listener,
-		d2=w2.document;
-
-
+		const dE=e,w2=((e=o.opened)===null||e.closed)?on(on((o.opened=w1.open()),'beforeunload',function(){o.opened=null;}).target,'click',inibite).target:e,d2=w2.document;
 		while(e=d2.firstChild){d2.removeChild(e);};
 		e=d1.doctype;
 		if(e!==null){d2.appendChild(e.cloneNode());};
@@ -37,4 +33,3 @@ log(1,this);
 	return sealed.go;
  }(o)))(o);
 }(window));
-//
