5b140778c0cead1a33ad3e5ce6bccb4684653498
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-05-15 18:51:41 +0200

    Update simplebookmarklet-x4.js

--- simplebookmarklet-x4.js
+++ simplebookmarklet-x4.js
@@ -6,9 +6,8 @@ void(function($){'use strict';
 	Call=w.Function.prototype.call,
 	aRawObj=Freeze(Call.bind(Create,null,null)),
 	aRawObjDescribed=Freeze(function(k){const o=k,f=aRawObj,d=f(),t=this;var p,i,e;for(p in o){i=d[p]=f(t);for(p in (e=o[p])){i[p]=e[p];};};return f(d);}.bind(function(u){var p=true;const f=Create,e=aRawObj(),s=Seal,o=s({value:p,writable:p,enumerable:p,configurable:false});for(p in o){e[p]=f(o);};e[p='value'][p]=u;return s(e);}())),
-	on=Freeze(function(o,s,f,b){const e=o;e.addEventListener(s,f,b===true);return {target:e,listener:f};}),
+	on=Freeze(function(o,s,f,b){const e=o;e.addEventListener(s,f,b===true);return e;}),
 	once=Freeze(function(o,s,f,b){const i=Freeze(function(x){f(x);o.removeEventListener(s,i);});on(s,i,b);}),
-	inibite=Freeze(function(evt){const e=evt;e.preventDefault();e.stopPropagation();return false;}),
 	console=w.console,log=Freeze(Call.bind(console.log,console)),
 	sealed=Seal(aRawObjDescribed({opened:{value:null},locked:{value:false},go:{writable:false,value:Freeze(function($){const o=sealed;if(o.locked===false){const w=$,d=w.document;o.locked=true;const f=process;if(d.readyState==='complete'){f({currentTarget:w});}else{once(w,'load',f);};};})}})),
 	process=Freeze(function(evt){
@@ -16,14 +15,16 @@ void(function($){'use strict';
 	 var e=d1.documentElement;
 	 if(e!==null){
 		e=d1.createDocumentFragment().appendChild(e.cloneNode(true));
-		e.normalize();
-		const dE=e,w2=((e=o.opened)===null||e.closed)?on(on((o.opened=w1.open()),'beforeunload',function(){o.opened=null;}).target,'click',inibite).target:e,d2=w2.document;
+		const dE=e,w2=((e=o.opened)===null||e.closed)?on(on((o.opened=w1.open('about:blank')),'beforeunload',function(){o.opened=null;}),'click',Freeze(function(evt){const e=evt;e.preventDefault();e.stopPropagation();return false;})):e,d2=w2.document;
 		while(e=d2.firstChild){d2.removeChild(e);};
-		e=d1.doctype;
-		if(e!==null){d2.appendChild(e.cloneNode());};
-		e=d2.appendChild(dE);
 
-log(1,this);
+		e=d1.doctype;if(e!==null){d2.appendChild(e.cloneNode());};
+
+		e=dE;
+		e.normalize();
+		e=d2.appendChild(e);
+
+		log(1,this);
 
 
 		w2.focus();
