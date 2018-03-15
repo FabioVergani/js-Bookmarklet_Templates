bd93ab87805a9bd156a98e177fc8311cf007bf93
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-03-15 18:03:31 +0100

    Update 2018.js

--- 2018.js
+++ 2018.js
@@ -1,29 +1,33 @@
 ($=>{
-	($.xxx||($.xxx=$=>{
-		const at=(e,s,x)=>{
-			const f=e.removeEventListener;
-			f(s,x);
-			e.addEventListener(s,x);
-			return f
-		};
-		let Run=false;
-		return($)=>{
-			if(Run!==true){
-				Run=true;
-				const d=$.document, f=$d=>{
-					//console.dir($);
-					console.info($d);
-					Run=false;
+	let k='MyUniqueID';
+	($[k]||($[k]=(($=>{
+		k=false;
+		return $=>{
+			if(k!==true){
+				k=true;//locked
+				const at=(e,s,x)=>{
+					const f=e.removeEventListener;
+					f(s,x),e.addEventListener(s,x);
+					return f;
+				},
+				d=$.document,
+				f=d=>{
+					//
+					console.dir(d);
+					//
+					k=false;
 				};
 				if(d.readyState!=='complete'){
-					const g=at($,'load',e=>{
-						g('load');
-						f(e.target);
-					});
+					const g=at($,'load',e=>{g('load');f(e.target)});
 				}else{
 					f(d);
 				};
 			};
 		};
-	}))($);
+	}))($)))($);
 })(window);
+//
+
+/*
+javascript:($=>{let k='MyUniqueID';($[k]||($[k]=(($=>{k=false;return$=>{if(k!==true){k=true;const at=(e,s,x)=>{const f=e.removeEventListener;f(s,x),e.addEventListener(s,x);return f},d=$.document,f=d=>{console.dir(d);k=false};if(d.readyState!=='complete'){const g=at($,'load',e=>{g('load');f(e.target)})}else{f(d)}}}}))($)))($)})(window);
+*/
