41bd4ed741996c89f6c967717fb2cb92f4579e03
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-05-10 17:56:45 +0200

    Create basicES6.js

---
+++ basicES6.js
@@ -0,0 +1,14 @@
+(d=>{
+	const ƒ=(evt)=>{
+		const d=evt.target;//w=d.defaultView
+		d.removeEventListener(evt.type,ƒ);
+		console.log('ƒ');
+	};
+	if(d.readyState!=='complete'){
+		console.warn('waitDom');
+		d.addEventListener('DOMContentLoaded',ƒ);
+	}else{
+		console.info('isReady');
+		ƒ({target:d})
+	}
+})(document);
