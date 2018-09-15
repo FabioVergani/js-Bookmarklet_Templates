72dcb5989c179a5d708ba13f954295ca18278b3e
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-15 12:20:05 +0200

    Update BookMarkletId.js

--- BookMarkletId.js
+++ BookMarkletId.js
@@ -1,12 +1,17 @@
 (w=>{
 	(w.BookMarkletId||(w.BookMarkletId=(w=>{
-		let opened,allowed=true;
+		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},
+		when=(d,c,i)=>{if(d.readyState!=='complete'){once(d,i!==1?'load':'DOMContentLoaded',c)}else{c({target:d})}};
+		let opened,allow=true;
 		return w=>{
-			if(allowed){
-				allowed=false;
-				console.info(666);
-				allowed=true;
+			if(allow){
+				allow=false;
+				const d=w.document;
+				when(d,loaded=>{
+					console.info('event:',loaded);
+					allow=true;
+				})
 			}
-		}	
-	})()))(w)
+		}
+	})(w)))(w)
 })(window);
