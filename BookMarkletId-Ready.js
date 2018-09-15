71e7b121d73d8da16aa10496049a5cb7fde301d2
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-15 14:26:50 +0200

    Update BookMarkletId-Ready.js

--- BookMarkletId-Ready.js
+++ BookMarkletId-Ready.js
@@ -1,5 +1,27 @@
 (w=>{
 	(w.BookMarkletId||(w.BookMarkletId=(w=>{
+		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},
+		ready=(w,c,i)=>{const d=w.document;if(d.readyState!=='complete'){if(i!==1){once(w,'DOMContentLoaded',c)}else{once(d,'load',c)}}else{c({target:d})}};
+		let opened,allow=true;
+		return w=>{
+			if(allow){
+				allow=false;
+				ready(w,loaded=>{
+					const d=loaded.target;
+
+ 
+
+					//Done:
+					allow=true
+				})
+			}
+		}
+	})(w)))(w)
+})(window);
+
+/*
+(w=>{
+	(w.BookMarkletId||(w.BookMarkletId=(w=>{
 		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},when=(w,c,i)=>{const d=w.document;if(d.readyState!=='complete'){if(i!==1){once(w,'load',c)}else{once(d,'DOMContentLoaded',c)}}else{c({target:d})}};
 		let opened,allow=true;
 		return w=>{
