0cd00175de1ab4ac503c6b4d2780e9d36c032fff
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-15 13:09:11 +0200

    Update BookMarkletId.js

--- BookMarkletId.js
+++ BookMarkletId.js
@@ -1,13 +1,12 @@
 (w=>{
 	(w.BookMarkletId||(w.BookMarkletId=(w=>{
 		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},
-		when=(d,c,i)=>{if(d.readyState!=='complete'){once(d,i!==1?'load':'DOMContentLoaded',c)}else{c({target:d})}};
+		when=(w,c,i)=>{const d=w.document;if(d.readyState!=='complete'){if(i!==1){once(w,'load',c)}else{once(d,'DOMContentLoaded',c)}}else{c({target:d})}};
 		let opened,allow=true;
 		return w=>{
 			if(allow){
 				allow=false;
-				const d=w.document;
-				when(d,loaded=>{
+				when(w,loaded=>{
 					console.info('event:',loaded);
 					allow=true;
 				})
@@ -15,6 +14,7 @@
 		}
 	})(w)))(w)
 })(window);
+
 
 
 
