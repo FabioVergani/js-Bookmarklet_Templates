8c13e1c14f86e8070f1d4a4f02c57e990e2644cf
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-16 15:01:13 +0200

    Update BookMarkletId-Ready.js

--- BookMarkletId-Ready.js
+++ BookMarkletId-Ready.js
@@ -1,41 +1,20 @@
 (w=>{
 	(w.BookMarkletId||(w.BookMarkletId=(w=>{
-		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},
-		ready=(w,c,i)=>{const d=w.document;if(d.readyState!=='complete'){if(i!==1){once(w,'DOMContentLoaded',c)}else{once(d,'load',c)}}else{c({target:d})}};
-		let opened,allow=true;
-		return w=>{
-			if(allow){
-				allow=false;
-				ready(w,loaded=>{
-					const d=loaded.target;
-
- 
-
-					//Done:
-					allow=true
-				})
-			}
-		}
-	})(w)))(w)
-})(window);
-
-/*
-(w=>{
-	(w.BookMarkletId||(w.BookMarkletId=(w=>{
-		const once=(e,s,f)=>{const c=o=>{e.removeEventListener(s,c);f(o)};e.addEventListener(s,c)},when=(w,c,i)=>{const d=w.document;if(d.readyState!=='complete'){if(i!==1){once(w,'load',c)}else{once(d,'DOMContentLoaded',c)}}else{c({target:d})}};
-		let opened,allow=true;
-		return w=>{
-			if(allow){
-				allow=false;
-				when(w,ready=>{
-					const d=ready.target;
-
-					console.info('event:',ready);
-
-					//Done:
-					allow=true
-				},1)
-			}
-		}
+		const Exp=[],
+		makeObject=w.Object.create,
+		dummyEvent=x=>makeObject(null,{target:{value:x},type:{value:false}}),
+		once=(e,s,c)=>{const f=o=>{e.removeEventListener(s,f);c(o)};e.addEventListener(s,f)},
+		isLoading=d=>d.readyState!=='complete',
+		onReady=(d,c)=>{if(isLoading(d)){once(d,'DOMContentLoaded',c)}else{c(dummyEvent(d))}},
+		isReady=d=>{
+			console.info('start');
+		},
+		isLoad=d=>{
+			console.info('done');
+			allow=true
+		},
+		atReady=e=>{const d=e.target,w=d.defaultView,f=e=>{isLoad(e.target)};isReady(d);if(e.type && isLoading(d)){once(w,'load',f)}else{f(dummyEvent(d))}};
+		let allow=true,opened;
+		return w=>{if(allow){allow=false;onReady(w.document,atReady)}}
 	})(w)))(w)
 })(window);
