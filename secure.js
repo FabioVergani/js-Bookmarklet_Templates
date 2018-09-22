ee2e5edf74382a35d8f1f72b5de94bfd55139070
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-22 18:33:37 +0200

    Create secure.js

---
+++ secure.js
@@ -0,0 +1,10 @@
+(w=>{const O=w.Object;let i='BookMarkletId',f=w[i];if('function'!==typeof f||O.getOwnPropertyDescriptor(w,i).writable){
+let counts=0;
+(f=O.defineProperty)(w,i,{value:f=f(w=>{if(allow){
+	allow=false;
+
+	console.info(++counts);
+
+	allow=true
+}},'id',{value:i}),configurable:true});
+with(f){allow=true}}else{const s=f.id;if('string'!==typeof s||i!==s){return}};f(w)})(window);
