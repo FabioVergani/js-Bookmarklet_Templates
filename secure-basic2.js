9eb1413d7acc20a311768cb936bf750aecb7c193
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-23 20:24:21 +0200

    Create secure-basic2.js

---
+++ secure-basic2.js
@@ -0,0 +1,4 @@
+(w=>{const O=w.Object;let i='BookMarkletId',f=w[i];if('function'!== typeof f||O.getOwnPropertyDescriptor(w,i).writable){O.defineProperty(w,i,{value:f=w=>{if(allow){allow=false;const d=w.document,c=loaded=>{
+alert(5);
+allow=true
+};if(d.readyState!=='complete'){once(w,'load',c)}else{c({target:d})}}},configurable:true});with(f){allow=true}};f(w)})(window);
