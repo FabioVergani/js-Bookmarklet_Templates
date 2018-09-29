7e3df82820bab604cf41c3f703e567c8f9d64b98
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-29 18:20:46 +0200

    Update scope1v2.js

--- scope1v2.js
+++ scope1v2.js
@@ -16,3 +16,23 @@
 })(window);
 //
 console.dir(window.BookMarkletId);
+/*
+
+(w=>{
+	let f=w.BookMarkletId;
+	if('function'!==typeof f){
+		with(f=w.BookMarkletId=w=>{
+			if(allow){
+				allow=false;
+				console.warn(++counts);
+				allow=true
+			}
+		}){
+			allow=true;
+			counts=0
+		}
+	};console.dir(f);
+	f(w)
+})(window);
+
+*/
