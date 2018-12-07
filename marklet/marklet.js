926ef77cfdb6c7d62d658804d82d6436edcf46ce
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-12-07 13:48:19 +0100

    Update marklet.js

--- marklet/marklet.js
+++ marklet/marklet.js
@@ -1,25 +1,13 @@
 void((w=>{
-	(w.marklet||(w.marklet=(w=>{
-		let allow;
-		const ready=o=>{
-			if(allow!==false){
-				allow=false;
-				const d=o.target;
-
-				console.dir(d);
+	(w.listLinks||(w.listLinks=(w=>{
+		let allowMarklet;
+		const once=(e,s,c)=>{const x=o=>{e.removeEventListener(s,x);c(o)};e.addEventListener(s,x)},
+		ready=event=>{console.dir(event);
+			if(allowMarklet!==false){
+				allowMarklet=false;
 
 			}
 		};
-		return w=>{
-			if(allow!==false){
-				const d=w.document,f=ready;
-				if(d.readyState!=='complete'){
-					const s='DOMContentLoaded',x=o=>{w.removeEventListener(s,x);f(o)};
-					w.addEventListener(s,x)
-				}else{
-					f({target:d})
-				}
-			}
-		}
+		return w=>{if(allowMarklet!==false){const d=w.document,c=ready;if(d.readyState!=='complete'){once(w,'DOMContentLoaded',c)}else{c({target:d,type:null})}}}
 	})(w)))(w)
 })(window));
